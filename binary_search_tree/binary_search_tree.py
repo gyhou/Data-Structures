4fa4d466c99eeff0ab47f8310941673bd7a8af8f
gyhou <47550192+gyhou@users.noreply.github.com>
2019-12-18 20:53:46 -0800

    Add files via upload

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -1,17 +1,99 @@
+import sys
+# sys.path.append('../queue_and_stack')
+from dll_queue import Queue
+from dll_stack import Stack
+
+
 class BinarySearchTree:
-  def __init__(self, value):
-    self.value = value
-    self.left = None
-    self.right = None
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
 
-  def insert(self, value):
-    pass
+    # Insert the given value into the tree
+    def insert(self, value):
+        if self.value:
+            if self.value > value:
+                if self.left:
+                    return self.left.insert(value)
+                else:
+                    self.left = BinarySearchTree(value)
+            else:
+                if self.right:
+                    return self.right.insert(value)
+                else:
+                    self.right = BinarySearchTree(value)
+        else:
+            self.value = BinarySearchTree(value)
 
-  def contains(self, target):
-    pass
+    # Return True if the tree contains the value
+    # False if it does not
+    def contains(self, target):
+        if target == self.value:
+            return True
+        else:
+            if self.value > target:
+                if self.left:
+                    return self.left.contains(target)
+                else:
+                    return False
+            else:
+                if self.right:
+                    return self.right.contains(target)
+                else:
+                    return False
 
-  def get_max(self):
-    pass
+    # Return the maximum value found in the tree
+    def get_max(self):
+        if self.right:
+            while self.right.right:
+                self.right = self.right.right
+            return self.right.value
+        else:
+            return self.value
 
-  def for_each(self, cb):
-    pass
+    # Call the function `cb` on the value of each node
+    # You may use a recursive or iterative approach
+    def for_each(self, cb):
+        """
+        Performs a traversal of every node in the tree, 
+        executing the passed-in callback function on each 
+        tree node value. There is a myriad of ways to perform 
+        tree traversal; in this case any of them should work.
+        """
+        cb(self.value)
+
+        if self.right:
+            self.right.for_each(cb)
+        if self.left:
+            self.left.for_each(cb)
+
+    # DAY 2 Project -----------------------
+
+    # Print all the values in order from low to high
+    # Hint:  Use a recursive, depth first traversal
+
+    def in_order_print(self, node):
+        pass
+
+    # Print the value of every node, starting with the given node,
+    # in an iterative breadth first traversal
+    def bft_print(self, node):
+        # Queue?
+        pass
+
+    # Print the value of every node, starting with the given node,
+    # in an iterative depth first traversal
+    def dft_print(self, node):
+        pass
+
+    # STRETCH Goals -------------------------
+    # Note: Research may be required
+
+    # Print In-order recursive DFT
+    def pre_order_dft(self, node):
+        pass
+
+    # Print Post-order recursive DFT
+    def post_order_dft(self, node):
+        pass
